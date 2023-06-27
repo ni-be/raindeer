6606f85cddfbdfede7fa7244af21ecd30b5e4b01
Nikolas Bertrand <code@nbertrand.dev>
2023-06-27 09:07:08 +0200

    added string to list converter, for single url or months input

--- src/downloader.py
+++ src/downloader.py
@@ -7,59 +7,75 @@ from global_var import YEARLY, MONTHLY, MONTHS, ROOT_DATA
 
 # download files from url_list YEARLY
 def downloader(url_list, months, dir):
+    ## IF url_list is a list do for loop if single item not
+    url_list, months = input_checker(url_list, months)
+
+    print(url_list, months, dir)
     for url in url_list:
         ## Get directory from url
-        interval_directory = url.split('/')[-1]
+        time_dir = url.split('/')[-3]
         ## Get sub directory from url
-        sub_directory = url.split('/')[-2]
-        ## Get filename from url
-        
-  
-        match interval_directory:
+        sub_dir = url.split('/')[-2]
+   
+        match time_dir:
             case "annual":
                 filename = url.split('/')[-1]
-                path = f"{dir}/{interval_directory}/{sub_directory}/{filename}"
-                valid_y_url = url_checker(url)
-                data_writer(valid_y_url, path)
-                 
+                path = f"{dir}/{time_dir}/{sub_dir}/{filename}"
+                url_checker_handler(path, url)
 
             case "monthly":
                 for n in months:
                     monthly_url = url.format(n)
                     filename = monthly_url.split('/')[-1]
-                    path = f"{dir}/{interval_directory}/{sub_directory}/{filename}"
-                    valid_m_url = url_checker(monthly_url)
-                    data_writer(valid_m_url, path)
+                    path = f"{dir}/{time_dir}/{sub_dir}/{filename}"
+                    url_checker_handler(path, monthly_url)
 
-                          
-def url_checker(url):
+
+def input_checker(url_list, months):
+    if isinstance(url_list, str) and isinstance(months, list):
+        url_list = string_list_converter(url_list)
+         
+    elif isinstance(url_list, list) and isinstance(months, str):
+        months = string_list_converter(months)
+    
+    elif isinstance(url_list, str) and isinstance(months, str):
+        url_list = string_list_converter(url_list)
+        months = string_list_converter(months)
+
+    else:
+        pass
+
+    return url_list, months
+
+
+def string_list_converter(input):
+    conv_list = []
+    conv_list += [input]
+    return conv_list
+
+
+def url_checker_handler(path, url):
     response = requests.get(url)
     if response.status_code == 200 and url.endswith('.txt'):
-        #print("OK")
         content = response.text
-        return content
+        data_writer(path, content)
     
     else:
         errorfile = url.split('/')[-1]
         print(f"Error downloading file: {errorfile}.")
         
 
-def data_writer(url, path):
+def data_writer(path, url):
     output_file = Path(path)
     output_file.parent.mkdir(exist_ok=True, parents=True)
     output_file.write_text(url)
     print(f"Downloaded '{path}' STATUS OK.")
 
 
-## ADD __IF__ to call it from terminal and from other programs
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process three inputs')
-    parser.add_argument('input1', help='1 or more URLs')
-    parser.add_argument('input2', help='Months wanted in a list  years')
-    parser.add_argument('input3', help='Where to downloard to')
-    args = parser.parse_args()
+#downloader("test", '01', "test")
+#downloader(['https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/air_temperature_mean/regional_averages_tm_{}.txt'], '01', "test")
 
-    try:
-        downloader(args.url, args.input2, args.input3)
-    except IndexError:
-        print("Invalid URL provided. Make sure the URL has enough components.")
+## ADD __IF__ to call it from terminal and from other programs
+#if __name__ == "__main__":
+    
+
--- src/global_var.py
+++ src/global_var.py
@@ -1,10 +1,10 @@
-YEARLY ={"https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/air_temperature_mean/regional_averages_tm_year.txt",
+YEARLY =["https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/air_temperature_mean/regional_averages_tm_year.txt",
          "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/precipitation/regional_averages_rr_year.txt",
-         "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/sunshine_duration/regional_averages_sd_year.txt"}
+         "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/annual/sunshine_duration/regional_averages_sd_year.txt"]
 
-MONTHLY = {"https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/air_temperature_mean/regional_averages_tm_{}.txt", 
+MONTHLY = ["https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/air_temperature_mean/regional_averages_tm_{}.txt", 
            "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/precipitation/regional_averages_rr_{}.txt",
-           "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/sunshine_duration/regional_averages_sd_{}.txt"}
+           "https://opendata.dwd.de/climate_environment/CDC/regional_averages_DE/monthly/sunshine_duration/regional_averages_sd_{}.txt"]
 
-MONTHS = {'01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'}
+MONTHS = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 ROOT_DATA = "../data"
