4ea0538b8a333a424b9c24db118861f385709215
Timo Wedding <wedding@uni-potsdam.de>
2023-07-03 12:42:27 +0200

    Added some testcases for fourier.py and between_years.py

--- src/test_funcs.py
+++ src/test_funcs.py
@@ -6,4 +6,23 @@ Created on Fri Jun 30 12:42:22 2023
 """
 
 import pytest
+import fourier
+import between_years
+import pandas as pd
 
+df = pd.read_csv('../data/monthly/precipitation/regional_averages_tm_all.csv')
+
+
+def test_fourier_input_table():
+    "Test for fourier analysis implementation for weather data"
+
+    with pytest.raises(AssertionError):
+        fourier.fourier_analysis(df, 'Deutschland', 'rain')
+
+
+def test_between_years():
+    "Test plot_between_years for behavior if non valid dates are given"
+
+    with pytest.raises(TypeError):
+        between_years.plot_between_years(df, [177001, 'a'],
+                                         'Deutschland', 'rain')
