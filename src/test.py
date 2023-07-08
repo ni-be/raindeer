44559054837dd3d0b2acb2ba278c3543bdd9649e
Nikolas Bertrand <code@nbertrand.dev>
2023-07-08 16:25:52 +0200

    Fourier analysis and Between years are integrated need work ....

--- src/test.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from dataframe_helper import dataframe_helper
-from utilities import yaml_reader
-
-months = yaml_reader('months')
-df = dataframe_helper("precipitation", "monthly", months, True)
-
-#print(df)
