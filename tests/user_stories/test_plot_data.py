90a1e1d67363fa87b5615be218f52d5d5030e815
Josephine Funken <funken@uni-bremen.de>
2023-07-17 12:17:08 +0200

    Added test

---
+++ tests/user_stories/test_plot_data.py
@@ -0,0 +1,15 @@
+import unittest
+from unittest.mock import patch
+from src.user_stories import plot_data
+
+
+class TestPlotData(unittest.TestCase):
+
+    @patch("matplotlib.pyplot.show")
+    def test_plot_data(self, mock_show):
+        print("\n Testing Plot Data [1/1]")
+        plot_data([1,2,3], [2,4,6], 'title', 'x', 'y')
+
+
+if __name__ == '__main__':
+    unittest.main()
