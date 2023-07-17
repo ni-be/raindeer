034d0b0039ac9d508598c0f12e3ca672bc9f6ab9
Josephine Funken <funken@uni-bremen.de>
2023-07-17 11:09:33 +0200

    Added unit test for user stories

--- tests/user_stories/test_fourier.py
+++ tests/user_stories/test_fourier.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import unittest
+from unittest.mock import patch
 import os
 import sys
 
@@ -12,10 +13,17 @@ from src.user_stories import fourier_analysis
 
 class TestFourierAnalysis(unittest.TestCase):
     def test_fourier_input_table(self):
-        print("\n Testing Fourier Analysis [1/1]")
+        print("\n Testing Fourier Analysis [1/2]")
         "Test for fourier analysis implementation for weather data"
         with self.assertRaises(AssertionError):
             fourier_analysis("precipitation", "monthly", 'Deutschland', 'rain')
+
+    @patch("matplotlib.pyplot.show")
+    def test_fourier_analysis(self, mock_show):
+        print("\n Testing Fourier Analysis [1/2]")
+        fourier_analysis('precipitation', 'annual',
+                         ['bayern', 'hessen'], 'sun')
+
 
 
 if __name__ == '__main__':
