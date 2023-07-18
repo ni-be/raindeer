fc880576c6a0fe85774e0792644b6c12f96e77b5
Joul1 <74826724+Joul1@users.noreply.github.com>
2023-07-18 14:01:51 +0200

    Finalised simple_plot unittest

--- tests/user_stories/test_simple_plot.py
+++ tests/user_stories/test_simple_plot.py
@@ -13,9 +13,9 @@ print()
 class TestSimplePlot(unittest.TestCase):
     def test_simple_plot1(self):
         print("\n Testing simple_plot function")
-        os.system(r"python raindeer\raindeer.py data\annual --mode=simple-plot --outfile results/user_stories/plots/simple_plot_temp.png -y 2000..2020 -b Brandenburg thueringen -m January..March -w precipitation")
-        #self.assertTrue(os.path.isfile(r"results/user_stories/plots/simple_plot_temp.png"))
-        #os.remove(r"results/user_stories/plots/simple_plot_temp.png")
+        os.system(r"""python raindeer/raindeer.py data/annual --outfile "results/user_stories/plots/simple_plot_temp.png" --mode=simple-plot -y 1981..2022 -b "brandenburg" "hessen" -w "precipitation" "sun" """)
+        self.assertTrue(os.path.isfile(r"results/user_stories/plots/simple_plot_temp.png"))
+        os.remove(r"results/user_stories/plots/simple_plot_temp.png")
 
 if __name__ == '__main__':
     unittest.main()
