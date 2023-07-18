285ef3141443c538e161cb75241638e39c5e53be
Nikolas Bertrand <code@nbertrand.dev>
2023-07-18 11:42:41 +0200

    90% coverage achieved in DWD_downloader

---
+++ tests/dwd_downloader/test_input_checker.py
@@ -0,0 +1,11 @@
+import unittest
+from raindeer.dwd_downloader import input_checker
+
+
+class TestInputChecker(unittest.TestCase):
+    def test_else_branch(self):
+        self.assertEqual(input_checker(123), ['123'])  
+    # The input is neither a string nor a list
+
+if __name__ == '__main__':
+    unittest.main()
